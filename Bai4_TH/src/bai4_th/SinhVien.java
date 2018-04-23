eb339425a65b6f20e48679729fa93e26cc24b9c0
thingoc <ngocntuk57@gmail.com>
2018-04-23 10:35:26 +0700

    $message

---
+++ Bai4_TH/src/bai4_th/SinhVien.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai4_th;
+
+/**
+ *
+ * @author USER
+ */
+public class SinhVien {
+    private String MSSV;
+    private String HoTen;
+    private String Namsinh;
+    private String Lop;
+
+    /**
+     * @return the MSSV
+     */
+    public String getMSSV() {
+        return MSSV;
+    }
+
+    /**
+     * @param MSSV the MSSV to set
+     */
+    public void setMSSV(String MSSV) {
+        this.MSSV = MSSV;
+    }
+
+    /**
+     * @return the HoTen
+     */
+    public String getHoTen() {
+        return HoTen;
+    }
+
+    /**
+     * @param HoTen the HoTen to set
+     */
+    public void setHoTen(String HoTen) {
+        this.HoTen = HoTen;
+    }
+
+    /**
+     * @return the Namsinh
+     */
+    public String getNamsinh() {
+        return Namsinh;
+    }
+
+    /**
+     * @param Namsinh the Namsinh to set
+     */
+    public void setNamsinh(String Namsinh) {
+        this.Namsinh = Namsinh;
+    }
+
+    /**
+     * @return the Lop
+     */
+    public String getLop() {
+        return Lop;
+    }
+
+    /**
+     * @param Lop the Lop to set
+     */
+    public void setLop(String Lop) {
+        this.Lop = Lop;
+    }
+    
+    
+}
